d0e766b6af0fe6b693a872b78acf58c1b9f3f970
Eric Elmoznino <eric.elmoznino@gmail.com>
2023-12-02 17:39:14 -0500

    Refactored polynomial regression into a more generic regression dataset/task

---
+++ data/regression.py
@@ -0,0 +1,149 @@
+from abc import ABC, abstractmethod
+from typing import Literal, Any, Iterable
+import numpy as np
+import torch
+from torch import FloatTensor
+from torch.utils.data import DataLoader
+from torchdata.datapipes.iter import IterDataPipe
+from lightning import LightningDataModule
+import warnings
+
+warnings.filterwarnings("ignore", message=".*does not have many workers.*")
+warnings.filterwarnings("ignore", message=".*`IterableDataset` has `__len__` defined.*")
+
+
+class RegressionDataModule(LightningDataModule):
+    RegressionKind = Literal["polynomial"]
+
+    def __init__(
+        self,
+        kind: RegressionKind,
+        min_context: int,
+        max_context: int,
+        batch_size: int = 128,
+        train_size: int = 10000,
+        val_size: int = 1000,
+        noise: float = 0.5,
+        ood: bool = True,
+        kind_kwargs: dict[str, Any] = {},
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+
+    def setup(self, stage=None):
+        RegressionDatasetCls = {
+            "polynomial": PolynomialRegressionDataset,
+        }[self.hparams.kind]
+        self.train_data = RegressionDatasetCls(
+            min_context=self.hparams.min_context,
+            max_context=self.hparams.max_context,
+            batch_size=self.hparams.batch_size,
+            data_size=self.hparams.train_size,
+            noise=self.hparams.noise,
+            ood=False,
+            **self.hparams.kind_kwargs,
+        )
+        self.val_data = RegressionDatasetCls(
+            min_context=self.hparams.min_context,
+            max_context=self.hparams.max_context,
+            batch_size=self.hparams.batch_size,
+            data_size=self.hparams.val_size,
+            noise=self.hparams.noise,
+            ood=self.hparams.ood,
+            **self.hparams.kind_kwargs,
+        )
+
+    def train_dataloader(self):
+        return DataLoader(self.train_data, batch_size=None)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_data, batch_size=None)
+
+
+class RegressionDataset(ABC, IterDataPipe):
+    def __init__(
+        self,
+        x_dim: int,
+        y_dim: int,
+        data_size: int,
+        min_context: int,
+        max_context: int,
+        batch_size: int = 128,
+        noise: float = 0.5,
+        ood: bool = False,
+    ) -> None:
+        super().__init__()
+        self.x_dim = x_dim
+        self.y_dim = y_dim
+        self.min_context = min_context
+        self.max_context = max_context
+        self.batch_size = batch_size
+        self.data_size = data_size
+        self.noise = noise
+        self.ood = ood
+        self.params_fixed = self.sample_function_params(100)
+
+    def sample_x(self, n_samples, n_context):
+        x_c = torch.randn(n_samples, n_context, self.x_dim)
+        if self.ood:
+            x_q_mean = 3 if np.random.random() > 0.5 else -3
+            x_q = x_q_mean + torch.randn(n_samples, self.x_dim) * 0.1
+        else:
+            x_q = x_c[:, 0] + torch.randn(n_samples, self.x_dim) * 0.1
+        return x_c, x_q.unsqueeze(1)
+
+    @abstractmethod
+    def sample_function_params(self, n_samples) -> FloatTensor:
+        pass
+
+    @abstractmethod
+    @torch.inference_mode()
+    def function(self, x, params) -> FloatTensor:
+        # x: (bsz, n_samples, x_dim)
+        # params: (bsz, ...) parameters of the function
+        # returns y: (bsz, n_samples, y_dim)
+        pass
+
+    def get_batch(self, n_context=None, indices=None):
+        if n_context is None:
+            n_context = np.random.randint(self.min_context, self.max_context + 1)
+        if indices is None:
+            params = self.sample_function_params(self.batch_size)
+        else:
+            params = self.params_fixed[indices]
+        x_c, x_q = self.sample_x(params.shape[0], n_context)
+        y_c, y_q = self.function(x_c, params), self.function(x_q, params)
+        y_c = y_c + self.noise * torch.randn_like(y_c)
+        x_q, y_q = x_q.squeeze(1), y_q.squeeze(1)
+        return (x_c, y_c), (x_q, y_q), params
+
+    def __len__(self):
+        return self.data_size // self.batch_size
+
+    def __iter__(self):
+        for _ in range(len(self)):
+            yield self.get_batch()
+
+
+class PolynomialRegressionDataset(RegressionDataset):
+    def __init__(
+        self,
+        order: int,
+        **kwargs,
+    ) -> None:
+        self.order = order
+        std = torch.linspace(1, 1 / order**2, order)
+        std = torch.cat([0.1 * torch.ones(1), std])  # Smaller y-intercepts
+        self.w_dist = torch.distributions.normal.Normal(torch.zeros(order + 1), std)
+        super().__init__(x_dim=1, y_dim=1, **kwargs)
+
+    def sample_function_params(self, n_samples) -> FloatTensor:
+        # Polynomial regression weights
+        return self.w_dist.rsample((n_samples,))
+
+    @torch.inference_mode()
+    def function(self, x, params) -> FloatTensor:
+        x = torch.cat([x**i for i in range(self.order + 1)], dim=-1)
+        params = params.unsqueeze(-1)
+        y = torch.bmm(x, params)
+        return y
--- tasks/regression.py
+++ tasks/regression.py
@@ -1,16 +1,17 @@
 import torch
 from lightning import LightningModule
+from einops import repeat
 from matplotlib import pyplot as plt
 from models.implicit import ImplicitModel
 from models.explicit import ExplicitModel, ExplicitModelWith, TransformerContext
 from tasks.utils import fig2img
 
 
-class PolynomialRegressionICL(LightningModule):
+class RegressionICL(LightningModule):
     def __init__(
         self,
         model: ImplicitModel | ExplicitModel,
-        order: int,
+        param_dim: int,
         lr: float = 1e-4,
     ):
         super().__init__()
@@ -19,42 +20,55 @@ class PolynomialRegressionICL(LightningModule):
 
         if isinstance(model, ExplicitModelWith):
             if isinstance(model.context_model, TransformerContext):
-                self.w_predictor = torch.nn.Linear(
-                    model.context_model.n_features, order + 1
+                self.param_predictor = torch.nn.Linear(
+                    model.context_model.n_features, param_dim
                 )
             else:
                 raise ValueError(
-                    f"Unknown context model: {type(model.context_model)}. Must specify how to get its z shape to build the W predictor."
+                    f"Unknown context model: {type(model.context_model)}. \
+                    Must specify how to get its z shape to build the parameter predictor."
                 )
         else:
-            self.w_predictor = None
+            self.param_predictor = None
 
     def training_step(self, batch, batch_idx):
-        (x_c, y_c), (x_q, y_q), w = batch
+        (x_c, y_c), (x_q, y_q), params = batch
         y_q_pred, z = self.model(x_c, y_c, x_q)
         y_q_loss = torch.nn.functional.mse_loss(y_q_pred, y_q)
         if z is not None:
-            w_pred = self.w_predictor(z.detach())
-            w_loss = torch.nn.functional.mse_loss(w_pred, w)
-            loss = y_q_loss + w_loss
-            self.log("train/w_MSE", w_loss, on_step=False, on_epoch=True)
+            params_pred = self.param_predictor(z.detach())
+            params_loss = torch.nn.functional.mse_loss(params_pred, params)
+            loss = y_q_loss + params_loss
+            self.log("train/params_MSE", params_loss, on_step=False, on_epoch=True)
         else:
             loss = y_q_loss
         self.log("train/MSE", y_q_loss, on_step=False, on_epoch=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        (x_c, y_c), (x_q, y_q), w = batch
+        (x_c, y_c), (x_q, y_q), params = batch
         y_q_pred, z = self.model(x_c, y_c, x_q)
         y_q_loss = torch.nn.functional.mse_loss(y_q_pred, y_q)
         if z is not None:
-            w_pred = self.w_predictor(z)
-            w_loss = torch.nn.functional.mse_loss(w_pred, w)
-            self.log("val/w_loss", w_loss, on_step=False, on_epoch=True)
+            params_pred = self.param_predictor(z)
+            params_loss = torch.nn.functional.mse_loss(params_pred, params)
+            self.log("val/params_MSE", params_loss, on_step=False, on_epoch=True)
         self.log("val/MSE", y_q_loss, on_step=False, on_epoch=True)
 
     def on_train_epoch_start(self):
-        if self.trainer.current_epoch % 10 == 0 and self.logger is not None:
+        if isinstance(self.model, ImplicitModel):
+            x_dim, y_dim = self.model.x_dim, self.model.y_dim
+        elif isinstance(self.model, ExplicitModelWith):
+            x_dim, y_dim = (
+                self.model.context_model.x_dim,
+                self.model.context_model.y_dim,
+            )
+        if (
+            self.trainer.current_epoch % 10 == 0
+            and x_dim == 1
+            and y_dim == 1
+            and self.logger is not None
+        ):
             self.eval()
             self.plot_model()
             self.train()
@@ -62,13 +76,13 @@ class PolynomialRegressionICL(LightningModule):
     @torch.inference_mode()
     def plot_model(self, n_examples=4):
         dataset = self.trainer.datamodule.train_data
-        (x_c, y_c), _, w = dataset.get_batch(
+        (x_c, y_c), _, params = dataset.get_batch(
             n_context=dataset.max_context, indices=range(n_examples)
         )
         x_c, y_c = x_c.to(self.device), y_c.to(self.device)
         ypred = []
         x = torch.linspace(-4, 4, 100)
-        y = dataset.function(x.unsqueeze(0).expand(n_examples, -1), w)
+        y = dataset.function(repeat(x, "c -> b c 1", b=n_examples), params).squeeze(-1)
         for x_q in x:
             x_q = x_q * torch.ones(n_examples, 1, device=self.device)
             y_q_pred, _ = self.model(x_c, y_c, x_q)
@@ -89,10 +103,10 @@ class PolynomialRegressionICL(LightningModule):
 
     def configure_optimizers(self):
         param_groups = [{"params": self.model.parameters()}]
-        if self.w_predictor is not None:
+        if self.param_predictor is not None:
             param_groups += [
                 {
-                    "params": self.w_predictor.parameters(),
+                    "params": self.param_predictor.parameters(),
                     "lr": self.hparams.lr * 10,
                 }
             ]
