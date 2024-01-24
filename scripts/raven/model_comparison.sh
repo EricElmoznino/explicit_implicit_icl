0a29bb9c3f8348982f63363fab7c4e3c4a30a579
EricElmoznino <eric.elmoznino@gmail.com>
2024-01-24 01:25:30 -0500

    Updated configs slightly based on hyperparameter search

--- scripts/raven/model_comparison.sh
+++ scripts/raven/model_comparison.sh
@@ -1,1 +1,1 @@
-python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/explicit_implicit_icl/logs experiment=raven/implicit,raven/explicit_tsf,raven/explicit_mlp seed=0
+python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/explicit_implicit_icl/logs experiment=raven/implicit,raven/explicit_tsf,raven/explicit_mlp seed=0,1,2,3,4
