34bff375521704ace418ce3b8ee155e84fab3c8e
Eric Elmoznino <eric.elmoznino@gmail.com>
2024-02-25 17:59:22 -0500

    Added configs for low-rank mlp regression

---
+++ scripts/lowrankmlp_regression/model_comparisons.sh
@@ -0,0 +1,1 @@
+python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/explicit_implicit_icl/logs experiment=lowrankmlp_regression/implicit,lowrankmlp_regression/explicit_tsf,lowrankmlp_regression/explicit_mlp ++experiment.data.context_style=same,near seed=0,1,2,3,4 ++experiment.callbacks=False ++experiment.data.noise=0.0
